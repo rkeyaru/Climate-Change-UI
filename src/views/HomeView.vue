9ba855bd5587b773b8ea8a0694fcef344b10aae3
rkeyaru <rkeyaru@gmail.com>
2023-07-10 00:37:26 +0530

    files committed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,20 @@
 <template>
   <main>
     <div class="container text-center mt-5">
-      <div class="text d-flex align-items-center justify-content-center">
+      <div class="text d-flex flex-column align-items-center justify-content-center">
         <div class="home-page-title w-50">
-          <h1 class="text-heading">Together, companies and consumers  can halt climate change.</h1>
+          <h1 class="text-heading px-5">
+            Together, companies and consumers can halt climate change.
+          </h1>
+        </div>
+        <div class="w-25">
+          <p class="home-page-text lead">
+            We help people consider the climate impacts of the brands they choose.
+          </p>
+        </div>
+        <div class="w-25 mt-2">
+          <button class="btn get-certify-button">GET CERTIFIED</button>
+          <a class="link-dark ms-3 browse-certified-brand" href="">Browse Certified Brands</a>
         </div>
       </div>
     </div>
@@ -13,10 +24,21 @@
 </template>
 <style scoped>
 .text-heading {
-  
   color: #001414;
-  font-family: Untitled Sans,Arial,sans-serif;
+  font-family: Untitled Sans, Arial, sans-serif;
   font-weight: bold;
-
+}
+.home-page-text {
+  font-size: 16px;
+}
+.get-certify-button {
+  font-size: 12px;
+  font-weight: 500;
+  background-color: #cca65b;
+  border-radius: 0;
+}
+.browse-certified-brand {
+  font-size: 10px;
+  text-underline-offset: 6px;
 }
 </style>
