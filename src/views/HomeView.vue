1875570abff6c85b4d2fc97e2042ee4a53d091b3
rkeyaru <rkeyaru@gmail.com>
2023-07-10 23:48:34 +0530

    files committed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,6 +18,28 @@
           <button class="btn get-certify-button">GET CERTIFIED</button>
           <a class="link-dark ms-3 browse-certified-brand" href="">Browse Certified Brands</a>
         </div>
+
+        <div class="mt-2 image-container">
+          <img src="@/assets/image1.png" alt="Save environment" class="w-100 home-page-image" />
+        </div>
+
+        <div class="w-50">
+          <div class="row">
+            <div class="col-6">
+              <h2 class="w-75 title">The world needs a price on carbon</h2>
+            </div>
+            <div class="col-6 image-description-container">
+              <p class="image-description upper lead">
+                Many governments have tried to find policy solutions.But their past attempts have
+                fallen short of the scale and impace needed.
+              </p>
+              <p class="image-description lead">
+                Carbon pricing would create incentives to lower carbon emissions and spur low carbon
+                innovation.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </main>
@@ -40,5 +62,28 @@
 .browse-certified-brand {
   font-size: 10px;
   text-underline-offset: 6px;
+}
+.image-container {
+  padding-right: 16%;
+}
+.home-page-image {
+  width: 115% !important;
+}
+.image-title {
+  font-family: 'Open Sans', sans-serif;
+  font-weight: bold;
+}
+
+.image-description {
+  font-family: verdana;
+  font-size: 12px;
+}
+.upper::first-letter {
+  font-size: 300%;
+  float: left;
+  line-height: 1;
+  display: inline-block;
+}
+.upper {
 }
 </style>
